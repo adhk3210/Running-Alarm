0c354a8ef9b838e30ff3bc65e0b0e70209de9d77
Edward Yoo <ehyoo97@gmail.com>
2015-09-05 02:23:56 -0400

    Initial Commit

---
+++ Running-Alarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running-Alarm
+//
+//  Created by Edward Yoo on 9/5/15.
+//  Copyright (c) 2015 Hohun Yoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
